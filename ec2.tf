b7e99cf62fba01094a66ff711bb81ab88ed354a7
Ubuntu <ubuntu@ip-172-31-19-224.ec2.internal>
2025-09-01 11:17:18 +0000

    updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.medium"
+  instance_type = "t3.medium"
   subnet_id     = aws_subnet.public1.id
   vpc_security_group_ids = [aws_security_group.PublicSG.id]
   key_name = aws_key_pair.kp.key_name
